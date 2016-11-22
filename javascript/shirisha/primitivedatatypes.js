349a0ec9a17bb1d9b36572ab4ab1b74a4b61581a
shirisha <skuncha@gmail.com>
2016-11-22 17:21:33 -0600

    My First javascript

---
+++ javascript/shirisha/primitivedatatypes.js
@@ -0,0 +1,33 @@
+/* Declaring the primitive datatypes in Javascript or ECMA Script5
+    there are five datatypes:number, string, boolean, null and undefined */
+
+//--1. Variables as number datatype
+var a = 10;
+var b = 45.99;
+
+//--2. Variables as string datatype
+var firstName = 'Shirisha';
+var lastName = 'Kuncha';
+
+//--3. Variables as 'Boolean' datatype
+var isHTMLLoaded = false;
+var isCSSLoaded = true;
+
+//--4. Variable as 'Null' datatype and holds the null value
+var x = null;
+
+//--5. Variable as 'undefined' datatype and means its not defined
+var y = undefined;
+
+// use the method console.log() to display the data
+console.log(a);
+console.log(b);
+console.log(firstName);
+console.log(lastName);
+console.log(isHTMLLoaded);
+console.log(isCSSLoaded);
+console.log(x);
+console.log(y);
+
+
+
