9cf3bd7069aba1372e46be79efc3350500bd24ca
shirisha <skuncha@gmail.com>
2016-12-01 17:10:28 -0600

    My First javascript

---
+++ javascript/11_30_2016/myfunction1.js
@@ -0,0 +1,28 @@
+// self executing anonymous function
+
+(function() {
+     function sayHello(){
+     alert("Hello tutorialspointexamples.com");
+     }
+
+	var Person = {
+
+		init: function() {
+			this.form = $('#form');
+			this.bindEvents();
+		},
+
+		bindEvents: function() {
+			this.form.on('submit', $.proxy(this.showName, this));
+		},
+
+		showName: function(event) {
+			event.preventDefault();
+
+			alert(this.form.find('input[type=text]').val());
+		}
+	}
+
+	Person.init();
+
+})();
