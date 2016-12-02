d27ee46b5cc2ee6366c76a308a73cccaf414de66
shirisha <skuncha@gmail.com>
2016-12-02 05:07:22 -0600

    oops in JS and class creation for reusability

---
+++ javascript/12__01__2016/class.js
@@ -0,0 +1,33 @@
+(function(){
+    /***this class is an object that can be reused and here the function is converted into an object
+        using the keyword 'this'*/
+    function x(){
+        this.firstName = 'Shirisha';
+        this.lastName = 'Kuncha';
+        this.rollNumber = 2;
+    }
+
+    /* function x() changed to object x as below
+        x = {
+            firstName: 'Shirisha',
+            lastName: 'Kuncha',
+            rollNumber: 2
+        };
+    */
+    /* creating the new instance of x */
+    var y = new x();
+    y.firstName = 'Tanvhika';
+    y.isPresent = function(){
+        return false;
+    };
+
+    var z = new x();
+
+    var test = y;
+    test.firstName = 'Dhaksh';
+
+
+    console.log(z);
+    console.log(y);
+
+})();
