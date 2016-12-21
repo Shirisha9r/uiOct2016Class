9a8fbab50251cecfca98cf7d96fc969112ca9f2d
shirisha <skuncha@gmail.com>
2016-12-21 00:08:48 -0600

    DOM_

--- javascript/12_19_2016-DOM/Shirisha/scripts.js
+++ javascript/12_19_2016-DOM/Shirisha/scripts.js
@@ -14,10 +14,21 @@
     console.log(c1);
     var d = document.querySelectorAll('[data-srikar="viswa"]');
     console.log(d);
-    console.log('#foo\bar')               // "#foobar" (\b is the backspace control character)
+    /*console.log('#foo\bar') */              // "#foobar" (\b is the backspace control character)
     /*document.querySelector('#foo\bar')    // Does not match anything*/
-    document.querySelector('#foo\\\\bar')    // escape
-
+    var e = document.querySelector('#foo\\\\bar')    // escape
+    console.log("print e");
+    console.log(e);
+    var p1 = document.body.querySelector('.viswa');
+    console.log("print p1");
+    console.log(p1);
+    var lis = document.querySelectorAll('ul.nav > li');
+    for (var i = 0; i < lis.length; i++) {
+      process(lis.item(i));
+    }
+    fuction process(lis.item(i)) {
+      return 'lis.length';
+    }
 
 
 
