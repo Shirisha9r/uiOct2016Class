56d60ef7925e2e6fc00f59ed6a2067df4f250902
shirisha <skuncha@gmail.com>
2017-01-04 03:49:21 -0600

    yeoman generator used to install grunt, bower,angular,karma etc.in a single shot

---
+++ angular/ShirishaProject/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name shirishaprojectApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the shirishaprojectApp
+ */
+angular.module('shirishaprojectApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ angular/ShirishaProject/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name shirishaprojectApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the shirishaprojectApp
+ */
+angular.module('shirishaprojectApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
